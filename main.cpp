229965be051a079339db7b9a59f3b79368da77e8
Ilya Trufakin <i.trufakin@gmail.com>
2024-10-27 03:12:35 +0800

    V0.02 DeepStream add

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data)
     switch (GST_MESSAGE_TYPE(msg))
     {
     case GST_MESSAGE_EOS:
-        g_print("Конец потока\n");
+        g_print("Thread end\n");
         g_main_loop_quit(loop);
         break;
 
@@ -22,7 +22,7 @@ static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data)
         gst_message_parse_error(msg, &error, &debug);
         g_free(debug);
 
-        g_printerr("Ошибка: %s\n", error->message);
+        g_printerr("Error: %s\n", error->message);
         g_error_free(error);
 
         g_main_loop_quit(loop);
@@ -37,8 +37,8 @@ static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data)
 
 int main(int argc, char *argv[])
 {
-    GstElement *pipeline, *source0, *source1, *nvvidconv0, *nvvidconv1, *queue0, *queue1, *compositor, *sink;
-    GstCaps *caps;
+    GstElement *pipeline, *source0, *source1, *nvvidconv0, *nvvidconv1, *queue0, *queue1, *nvvidconv2, *nvvidconv3, *compositor, *sink;
+    GstCaps *caps_nv12, *caps_rgba;
     GstPad *sinkpad0, *sinkpad1;
     GstBus *bus;
     GMainLoop *loop;
@@ -61,13 +61,16 @@ int main(int argc, char *argv[])
     queue0 = gst_element_factory_make("queue", "queue0");
     queue1 = gst_element_factory_make("queue", "queue1");
 
+    nvvidconv2 = gst_element_factory_make("nvvidconv", "nvvidconv2"); // Конвертер для RGBA для камеры 0
+    nvvidconv3 = gst_element_factory_make("nvvidconv", "nvvidconv3"); // Конвертер для RGBA для камеры 1
+
     compositor = gst_element_factory_make("nvcompositor", "compositor");
 
     sink = gst_element_factory_make("nvoverlaysink", "sink");
 
-    if (!pipeline || !source0 || !source1 || !nvvidconv0 || !nvvidconv1 || !queue0 || !queue1 || !compositor || !sink)
+    if (!pipeline || !source0 || !source1 || !nvvidconv0 || !nvvidconv1 || !queue0 || !queue1 || !nvvidconv2 || !nvvidconv3 || !compositor || !sink)
     {
-        g_printerr("Не удалось создать элементы\n");
+        g_printerr("Elements not created\n");
         return -1;
     }
 
@@ -75,52 +78,75 @@ int main(int argc, char *argv[])
     g_object_set(G_OBJECT(source0), "sensor-id", 0, NULL);
     g_object_set(G_OBJECT(source1), "sensor-id", 1, NULL);
 
-    /* Установка разрешения и частоты кадров */
-    caps = gst_caps_from_string("video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1");
+    /* Установка разрешения и частоты кадров для NV12 */
+    caps_nv12 = gst_caps_from_string("video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=60/1");
+    caps_rgba = gst_caps_from_string("video/x-raw, format=RGBA");
 
     /* Добавление элементов в пайплайн */
-    gst_bin_add_many(GST_BIN(pipeline), source0, nvvidconv0, queue0, source1, nvvidconv1, queue1, compositor, sink, NULL);
+    gst_bin_add_many(GST_BIN(pipeline), source0, nvvidconv0, queue0, nvvidconv2, source1, nvvidconv1, queue1, nvvidconv3, compositor, sink, NULL);
 
     /* Связывание источника 0 */
-    if (!gst_element_link_filtered(source0, nvvidconv0, caps))
+    if (!gst_element_link(source0, nvvidconv0))
     {
-        g_printerr("Не удалось связать source0 с nvvidconv0\n");
+        g_printerr("source0 with nvvidconv0 no bind\n");
         return -1;
     }
+
+
+    // if (!gst_element_link_filtered(source0, nvvidconv0, caps_nv12))
+    // {
+    //     g_printerr("source0 with nvvidconv0 no bind\n");
+    //     return -1;
+    // }
     if (!gst_element_link(nvvidconv0, queue0))
     {
-        g_printerr("Не удалось связать nvvidconv0 с queue0\n");
+        g_printerr("nvvidconv0 with queue0 no bind\n");
+        return -1;
+    }
+    if (!gst_element_link_filtered(queue0, nvvidconv2, caps_rgba))
+    {
+        g_printerr("queue0 with nvvidconv2 no bind\n");
         return -1;
     }
 
     /* Связывание источника 1 */
-    if (!gst_element_link_filtered(source1, nvvidconv1, caps))
+    if (!gst_element_link(source1, nvvidconv1))
     {
-        g_printerr("Не удалось связать source1 с nvvidconv1\n");
+        g_printerr("source0 with nvvidconv0 no bind\n");
         return -1;
     }
+    // if (!gst_element_link_filtered(source1, nvvidconv1, caps_nv12))
+    // {
+    //     g_printerr("source1 with nvvidconv1 no bind\n");
+    //     return -1;
+    // }
     if (!gst_element_link(nvvidconv1, queue1))
     {
-        g_printerr("Не удалось связать nvvidconv1 с queue1\n");
+        g_printerr("nvvidconv1 with queue1 no bind\n");
+        return -1;
+    }
+    if (!gst_element_link_filtered(queue1, nvvidconv3, caps_rgba))
+    {
+        g_printerr("queue1 with nvvidconv3 no bind\n");
         return -1;
     }
 
-    /* Связывание очередей с композитором */
-    if (!gst_element_link(queue0, compositor))
+    /* Связывание конвертеров RGBA с композитором */
+    if (!gst_element_link(nvvidconv2, compositor))
     {
-        g_printerr("Не удалось связать queue0 с композитором\n");
+        g_printerr("nvvidconv2 with compositor no bind\n");
         return -1;
     }
-    if (!gst_element_link(queue1, compositor))
+    if (!gst_element_link(nvvidconv3, compositor))
     {
-        g_printerr("Не удалось связать queue1 с композитором\n");
+        g_printerr("nvvidconv3 with compositor no bind\n");
         return -1;
     }
 
     /* Связывание композитора с выходом */
     if (!gst_element_link(compositor, sink))
     {
-        g_printerr("Не удалось связать композитор с выводом\n");
+        g_printerr("compositor with output no bind\n");
         return -1;
     }
 
@@ -157,7 +183,8 @@ int main(int argc, char *argv[])
     /* Очистка ресурсов */
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
-    gst_caps_unref(caps);
+    gst_caps_unref(caps_nv12);
+    gst_caps_unref(caps_rgba);
     g_main_loop_unref(loop);
 
     return 0;
